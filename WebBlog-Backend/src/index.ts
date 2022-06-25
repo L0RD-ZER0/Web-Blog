b087f6d551f222ea5603df464f368885c273a8eb
Zero <ackerhon@gmail.com>
2022-06-25 18:29:19 +0530

    Backend Setup

---
+++ WebBlog-Backend/src/index.ts
@@ -0,0 +1,20 @@
+import app from './app';
+
+if (process.env.NODE_ENV === 'development')  {
+    /* Development */
+}
+
+
+/**
+ * Start Express Server.
+ */
+const server = app.listen(app.get('port'), () => {
+    console.log(
+        "  App is running at http://localhost:%d in %s mode",
+        app.get("port"),
+        app.get("env")
+    );
+    console.log("  Press CTRL-C to stop\n");
+});
+
+export default server;
